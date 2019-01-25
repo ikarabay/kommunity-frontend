3aedc4811a573a0a42b2d018c6dadafd111b3785
Selman Kahya <selmankahya@hotmail.com>
2019-01-25 07:47:51 -0800

    adding todo comment

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+// TODO is it safe to keep this here?
 export const RECAPTCHA_API_KEY = '6Lfxa3kUAAAAAA2Urk0EXbI1cUKU3xUCvCG6HEIY'; // bariscc
 export const mailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$';
 export const EMAIL_LIST_IDS = { BETA_SIGNUP: '6746304' };
